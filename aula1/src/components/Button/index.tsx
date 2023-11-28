c01b4952739113ddf492128838143049f1bc9542
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2023-11-27 22:09:53 -0300

    Aulas 1 e 2

---
+++ aula1/src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import { Button as NativeButton } from "native-base";
+
+interface Props {
+    content: string;
+    variation: string;
+    handleClick: () => void
+}
+
+export default function Button({content, variation, handleClick}:Props) {
+    return (
+        <NativeButton bg={`${variation}.100`} mt={3} onPress={handleClick}>
+                {content}
+        </NativeButton>
+    )
+}
