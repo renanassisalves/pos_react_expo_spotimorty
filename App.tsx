021ef00dcdcf5d078824d15687d5f572e6009f1a
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2023-12-18 20:08:12 -0300

    Adicionado push e sample de push como tela principal apenas para fins de teste

--- App.tsx
+++ App.tsx
@@ -1,60 +1,113 @@
-import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider } from "native-base";
-import THEME from "./src/theme";
-import UserContext, { IUser } from "./src/context/user";
-import Wrapper from "./src/screens/Wrapper";
-import { useEffect, useState } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { MMKV } from "react-native-mmkv";
-import Cadastro from "./src/screens/Cadastro";
-import Login from "./src/screens/Login";
-import Home from "./src/screens/Musicas";
-import Menu from "./src/screens/Menu";
-import Musicas from "./src/screens/Musicas";
-import RickMorty from "./src/screens/RickMorty";
-import { iCharacter, CharacterContextProvider } from "./src/context/characters";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 
-export const storage = new MMKV({
-  id: "movieapp",
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
 });
 
+
+// Can use this function below or use Expo's Push Notification Tool from: https://expo.dev/notifications
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  })
+  // .then((resposta) => {console.log(JSON.stringify(resposta))});
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig.extra.eas.projectId,
+    });
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token.data;
+}
+
 export default function App() {
-  const [user, setUser] = useState<IUser | null>(null);
-
-
-  useEffect(() => {
-    if (user != null) {
-      storage.set("user", JSON.stringify(user));
-    }
-  }, [user]);
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   useEffect(() => {
-    const userDb = storage.getString("user");
-    if (userDb) {
-      setUser(JSON.parse(userDb));
-    }
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log("Notificação recebida");
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, []);
 
-  const Stack = createNativeStackNavigator();
-
   return (
-    <NativeBaseProvider theme={THEME}>
-      <UserContext.Provider value={{ user: user, setUser }}>
-        <CharacterContextProvider>
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-              <Stack.Screen name="Login" component={Login} />
-              <Stack.Screen name="Cadastrar" component={Cadastro} />
-              <Stack.Screen name="Menu" component={Menu} />
-              <Stack.Screen name="Musicas" component={Musicas} />
-              <Stack.Screen name="RickMorty" component={RickMorty} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </CharacterContextProvider>
-      </UserContext.Provider>
-    </NativeBaseProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification("ExponentPushToken[hpZn0qCeQFIk_sOoLwlQ8d]");
+        }}
+      />
+    </View>
   );
 }
